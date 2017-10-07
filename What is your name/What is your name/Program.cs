7a5ef6932bb9ce90c0462f04c72cc5a82d0b4f2a
RadoslavStamboliev <stamboliev98@gmail.com>
2017-10-07 10:44:39 +0300

    Add exercise

---
+++ What is your name/What is your name/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_is_your_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
